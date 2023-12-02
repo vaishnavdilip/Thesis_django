4ec52d27b08c4c53cc004c46a31bb3ae443368a6
Vaishnav Dilip <100198464+vaishnavdilip@users.noreply.github.com>
2023-12-02 19:04:24 +0100

    Update dashboard template and utils.py

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -3,6 +3,99 @@ import numpy as np
 from py2neo import Graph
 import plotly.express as px
 import plotly.graph_objects as go
+import plotly.offline as pyo
 
 graph = Graph("bolt://localhost:7687", auth=("neo4j", "neo4jneo4j"))
 
+def sector_dist():
+    query = """
+    MATCH (c:Company)
+    RETURN c.sector as Sector, count(c) as count
+    """
+
+    a = graph.run(query).to_data_frame()
+
+    fig = px.pie(a, values='count', names='Sector', title='Sector distribution', height=500, width=700)
+    plot_html = pyo.plot(fig, output_type='div')
+
+    return plot_html
+
+def supplier_year_dist():
+    query = """
+    MATCH ()-[r:SUPPLIES_TO]->()
+    RETURN date(r.date).year as Year, count(r) as count
+    """
+    
+    a = graph.run(query).to_data_frame()
+
+    fig = px.bar(a, x='Year', y='count',  height=500, width=700)
+
+    fig.update_layout(
+        title='Number of supplier relations per year',
+        xaxis_title='Year',
+        yaxis_title='Number of supplier relations',
+    )
+
+    plot_html = pyo.plot(fig, output_type='div')
+    return plot_html
+
+def competes_year_dist():
+    query = """
+    MATCH ()-[r:COMPETES]->()
+    RETURN date(r.date).year as Year, count(r) as count
+    """
+
+    a = graph.run(query).to_data_frame()
+
+    fig = px.bar(a, x='Year', y='count',  height=500, width=700)
+
+    fig.update_layout(
+        title='Number of supplier relations per year',
+        xaxis_title='Year',
+        yaxis_title='Number of supplier relations',
+    )
+
+    plot_html = pyo.plot(fig, output_type='div')
+    return plot_html
+def partners_year_dist():
+    query = """
+    MATCH ()-[r:PARTNERS]->()
+    RETURN date(r.date).year as Year, count(r) as count
+    """
+
+    a = graph.run(query).to_data_frame()
+    fig = px.bar(a, x='Year', y='count',  height=500, width=700)
+
+    fig.update_layout(
+        title='Number of supplier relations per year',
+        xaxis_title='Year',
+        yaxis_title='Number of supplier relations',
+    )
+
+    plot_html = pyo.plot(fig, output_type='div')
+    return plot_html
+
+def country_dist():
+    query = """
+    MATCH (c:Company)
+    RETURN c.country_code as country, count(c) as count
+    """
+
+    a = graph.run(query).to_data_frame()
+
+    fig = px.choropleth(a, locations="country",
+                    color="count",
+                    hover_name="country", 
+                    color_continuous_scale=px.colors.sequential.Plasma,
+                    height=500, width=1000,)
+
+    fig.update_layout(
+        coloraxis_colorbar=dict(
+        title="Number of companies per country",
+        len=1
+        ),
+        title_text = 'Availability of company data based on country'
+    )
+
+    plot_html = pyo.plot(fig, output_type='div')
+    return plot_html
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render
 from django.http import JsonResponse
 # from dashboard.models import Order
 from django.core import serializers
+from .utils import sector_dist, supplier_year_dist, competes_year_dist, partners_year_dist, country_dist
 
 # Create your views here.
 
 def dashboard_with_pivot(request):
-    return render(request, 'dashboard_with_pivot.html', {})
+    sector_plot = sector_dist()
+    competes_plot = competes_year_dist()
+    partner_plot = partners_year_dist()
+    supplier_plot = supplier_year_dist()
+    country_plot = country_dist()
+    return render(request, 'dashboard_with_pivot.html', {'sector_plot' : sector_plot, 'competes_plot' : competes_plot, 'partner_plot' : partner_plot, 'supplier_plot' : supplier_plot, 'country_plot' : country_plot})
 
 # def pivot_data(request):
 #     dataset = Order.objects.all()
